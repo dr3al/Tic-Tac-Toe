e48a1fbc5edb66269d8aebf5d794ed4991a04a42
dr3al <47189738+dr3al@users.noreply.github.com>
2019-05-02 20:09:04 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+#Tic-Tac-Toe
+
+import os
+import random
+
+
+flat = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+s = os.sys.platform
+
+
+def prnt():
+    print('', flat[0], '|', flat[1], '|', flat[2], ' ')
+    print('--- --- ---')
+    print('', flat[3], '|', flat[4], '|', flat[5], ' ')
+    print('--- --- ---')
+    print('', flat[6], '|', flat[7], '|', flat[8], ' ')
+
+def a_turn():
+    x = int(input())
+    if flat[x-1] != ' ':
+        print('')
+        print('Эта клетка занята!')
+        print('')
+        a_turn()
+    else:
+        if x == 1:
+            flat[0] = 'X'
+        elif x == 2:
+            flat[1] = 'X'
+        elif x == 3:
+            flat[2] = 'X'
+        elif x == 4:
+            flat[3] = 'X'
+        elif x == 5:
+            flat[4] = 'X'
+        elif x == 6:
+            flat[5] = 'X'
+        elif x == 7:
+            flat[6] = 'X'
+        elif x == 8:
+            flat[7] = 'X'
+        elif x == 9:
+            flat[8] = 'X'
+        else:
+            print('Такой ячейки не существует')
+
+def b_turn():
+    x = random.randint(1,9)
+    if flat[x-1] != ' ':
+        b_turn()
+    else:
+        if x == 1:
+            flat[0] = 'O'
+        elif x == 2:
+            flat[1] = 'O'
+        elif x == 3:
+            flat[2] = 'O'
+        elif x == 4:
+            flat[3] = 'O'
+        elif x == 5:
+            flat[4] = 'O'
+        elif x == 6:
+            flat[5] = 'O'
+        elif x == 7:
+            flat[6] = 'O'
+        elif x == 8:
+            flat[7] = 'O'
+        elif x == 9:
+            flat[8] = 'O'
+
+def end_g():
+    if flat[0] == flat[1] == flat[2] == 'X' or flat[3] == flat[4] == flat[5] == 'X' or flat[6] == flat[7] == flat[8] == 'X' or flat[0] == flat[3] == flat[6] == 'X' or flat[1] == flat[4] == flat[7] == 'X' or flat[2] == flat[5] == flat[8] == 'X' or flat[0] == flat[4] == flat[8] == 'X' or flat[2] == flat[4] == flat[6] == 'X':
+        clrscr()
+        print('Поздравляем! Вы победили!')
+        return 0
+    elif flat[0] == flat[1] == flat[2] == 'O' or flat[3] == flat[4] == flat[5] == 'O' or flat[6] == flat[7] == flat[8] == 'O' or flat[0] == flat[3] == flat[6] == 'O' or flat[1] == flat[4] == flat[7] == 'O' or flat[2] == flat[5] == flat[8] == 'O' or flat[0] == flat[4] == flat[8] == 'O' or flat[2] == flat[4] == flat[6] == 'O':
+        clrscr()
+        print('Сожалеем, вы проиграли.')
+        return 0
+    elif flat[0] != ' ' and flat[1] != ' ' and flat[2] != ' ' and flat[3] != ' ' and flat[4] != ' ' and flat[5] != ' ' and flat[6] != ' ' and flat[7] != ' ' and flat[8] != ' ':
+        clrscr()
+        print('Оу, это ничья!')
+        return 0
+    else:
+        game()
+
+def clrscr():
+    if s[0] == 'w':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def game():
+    a_turn()
+    if ' ' not in flat:
+        end_g()
+    else:
+        clrscr()
+        prnt()
+        b_turn()
+        clrscr()
+        prnt()
+        end_g()
+prnt()
+game()
