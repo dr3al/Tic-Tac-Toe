d3f436995e6c4fd138cdb4cef1a35a403893f899
Gleb <gleb270@mail.ru>
2019-10-26 15:20:05 +0700

    some fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import random
 
 
-flat = [' ']*9
+flat = [' '] * 9
 combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 s = os.sys.platform
 
@@ -18,23 +18,23 @@ def prnt():
 
 def a_turn():
     x = input()
-    if not x.isdigit() or not 1<=int(x)<=9 or flat[int(x)-1] != ' ':
+    if not x.isdigit() or not 1 <= int(x) <= 9 or flat[int(x) - 1] != ' ':
         print('\nТак ходить нельзя\n')
         a_turn()
     else:
-        flat[int(x)-1] = 'x'
+        flat[int(x) - 1] = 'x'
 
 
 def b_turn():
-    x = random.randint(1,9)
-    if flat[x-1] != ' ':
+    x = random.randint(1, 9)
+    if flat[x - 1] != ' ':
         b_turn()
     else:
-        flat[x-1] = 'O'
+        flat[x - 1] = 'O'
 
 def check_win(sym):
     for i, j, k in combinations:
-        if flat[i]==flat[j]==flat[k]==sym:
+        if flat[i] == flat[j] == flat[k] == sym:
             return True
     return False
 
